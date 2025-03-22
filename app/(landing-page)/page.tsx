1f5995875102b9203f31dde2730b4c9e830ede6b
Vo1denz <thunderplayz2325@gmail.com>
2025-03-22 21:55:16 +0530

    Added Navbar

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from 'react'
+import Navbar from "@/app/(landing-page)/components/Navbar";
 
-function page() {
+const Page = () => {
+  useEffect(() => {
+    const loadLocomotiveScroll = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll();
+    };
+
+    loadLocomotiveScroll();
+  }, []);
+
   return (
-    <div>page</div>
-  )
-}
+    <div className="w-full min-h-screen text-white" style={{ backgroundColor: "#E5F4DD" }}>
+      <Navbar />
+    </div>
+  );
+};
 
-export default page
+export default Page;
