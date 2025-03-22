d9ef47346a8eb6d5dc81b6296ba9482f3956b718
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-23 01:37:42 +0530

    Update convex schema for Mood Entries

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,10 +2,26 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+
+  
   users: defineTable({
     name: v.string(),
     email: v.string(),
     image: v.optional(v.string()),
     clerkId: v.string(),
   }).index("by_clerk_id", ["clerkId"]),
+
+
+  moodEntries: defineTable({
+    userId: v.string(),
+    date: v.string(),
+    mood: v.number(),
+    sleep: v.object({ hours: v.number(), quality: v.number() }),
+    anxiety: v.number(),
+    stress: v.number(),
+    activities: v.array(v.string()),
+    note: v.optional(v.string()),
+  }).index("by_user", ["userId"]),
+
+
 });
