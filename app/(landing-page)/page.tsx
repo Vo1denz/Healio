61de6e3b54d26e3ba4fc30631bdf0636d46002ea
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-22 23:07:37 +0530

    Done with landing page

--- app/(landing-page)/page.tsx
+++ app/(landing-page)/page.tsx
@@ -1,24 +1,16 @@
 "use client";
 
 import Navbar from "@/app/(landing-page)/components/Navbar";
-import React, { useEffect } from "react";
-import LandingPage from "./components/LandingPade";
-import Marquee from "./components/Marquee";
+import React from "react";
 import About from "./components/About";
-import Eyes from "./components/Eyes";
 import Featured from "./components/Featured";
-import Steps from "./components/Steps";
+import LandingPage from "./components/LandingPage";
+import Marquee from "./components/Marquee";
+import Eyes from "./components/Eyes";
 import Footer from "./components/Footer";
+import Steps from "./components/Steps";
 
 const Page = () => {
-  useEffect(() => {
-    const loadLocomotiveScroll = async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      new LocomotiveScroll();
-    };
-
-    loadLocomotiveScroll();
-  }, []);
 
   return (
     <div className="w-full min-h-screen text-white" style={{ backgroundColor: "#E5F4DD" }}>
