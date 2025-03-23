003d36264773ae2996de0ad279a802805ecdc8e6
Shwejan Kumar Enjamuri <shwejannn@gmail.com>
2025-03-23 09:03:12 +0530

    Added messages actions

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -17,6 +17,7 @@ import type * as forum from "../forum.js";
 import type * as goals from "../goals.js";
 import type * as http from "../http.js";
 import type * as journals from "../journals.js";
+import type * as messages from "../messages.js";
 import type * as moodEntries from "../moodEntries.js";
 import type * as rooms from "../rooms.js";
 import type * as users from "../users.js";
@@ -34,6 +35,7 @@ declare const fullApi: ApiFromModules<{
   goals: typeof goals;
   http: typeof http;
   journals: typeof journals;
+  messages: typeof messages;
   moodEntries: typeof moodEntries;
   rooms: typeof rooms;
   users: typeof users;
